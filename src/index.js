a9089a8034156f05ae5ed6c9d63f1e066d80e0e9
FranKydeSU <franapasin.s@gmail.com>
2021-12-02 01:13:01 +0700

    Init commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express()
 const server = http.createServer(app) // refactoring for supporting socket.io
 const io = socketio(server) // require socket function
 
-const port = process.env.PORT | 3000
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
